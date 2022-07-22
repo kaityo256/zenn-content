d3a4caf636b20db8ce4f8c5f2b28ee6ed0227871
H. Watanabe <kaityo@users.sourceforge.jp>
2022-07-22 18:31:14 +0900

    updates

--- articles/bolzmann_machine/prob.py
+++ articles/bolzmann_machine/prob.py
@@ -2,15 +2,17 @@ import itertools
 import numpy as np
 from fractions import Fraction
 
-w_a = Fraction(3,1)
-w_b = Fraction(1,2)
-w_c = Fraction(1,2)
+w_a = Fraction(4,1)
+w_b = Fraction(1,4)
+w_c = Fraction(1,4)
 
-w_ac = Fraction(1,9)
-w_bc = Fraction(4,1)
+w_ac = Fraction(1,28)
+w_bc = Fraction(28,1)
 
 print("A B C")
 sum = Fraction(0,1)
+sum_c0 = Fraction(0,1)
+sum_c1 = Fraction(0,1)
 for a,b,c in itertools.product([0,1],[0,1],[0,1]):
     prob = 1
     if a:
@@ -23,9 +25,13 @@ for a,b,c in itertools.product([0,1],[0,1],[0,1]):
         prob *= w_ac
     if b and c:
         prob *= w_bc
-    print(f"{a} {b} {c} {prob}")
+    print(f"{a} {b} {c} {prob*28}")
+    if c:
+        sum_c1 += prob
+    else:
+        sum_c0 += prob
     sum += prob
-print(sum)
+print(sum_c1/(sum_c0+sum_c1))
 print("A C")
 for a,c in itertools.product([0,1],[0,1]):
     prob = 1
