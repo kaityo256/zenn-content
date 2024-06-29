571cc320441b0a1913a3ac42645d4f5d2959fc4e
H. Watanabe <kaityo@users.sourceforge.jp>
2024-06-29 21:33:44 +0900

    updates

---
+++ articles/programming_howto/lammps.py
@@ -0,0 +1,48 @@
+class Atom:
+    def __init__(self, x, y, z, vx, vy, vz):
+        self.x = x
+        self.y = y
+        self.z = z
+        self.vx = vx
+        self.vy = vy
+        self.vz = vz
+
+    def __str__(self):
+        return f"{self.x} {self.y} {self.z} {self.vx} {self.vy} {self.vz} "
+
+
+def read_atoms(f):
+    atoms = []
+    for line in f:
+        if "ITEM: TIMESTEP" in line:
+            return atoms
+        a = line.split()
+        x = float(a[2])
+        y = float(a[3])
+        z = float(a[4])
+        vx = float(a[5])
+        vy = float(a[6])
+        vz = float(a[7])
+        atom = Atom(x, y, z, vx, vy, vz)
+        atoms.append(atom)
+
+
+def read_file(filename):
+    frames = []
+    with open(filename) as f:
+        for line in f:
+            if "ITEM: ATOMS" in line:
+                atoms = read_atoms(f)
+                frames.append(atoms)
+    return frames
+
+
+def main():
+    filename = "sample.lammpstrj"
+    frames = read_file(filename)
+    for atoms in frames:
+        print(len(atoms))
+
+
+if __name__ == "__main__":
+    main()
