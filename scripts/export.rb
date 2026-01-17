072ba23eb051b61e882412eb1da8f667367c7a3b
H. Watanabe <kaityo@users.sourceforge.jp>
2026-01-17 22:58:42 +0900

    updates

--- scripts/export.rb
+++ scripts/export.rb
@@ -2,7 +2,7 @@
 
 require "json"
 
-def load_zenn_json(path)
+def load_zenn_json(path, articles)
   unless File.file?(path)
     raise ArgumentError, "File not found: #{path}"
   end
@@ -15,13 +15,58 @@ def load_zenn_json(path)
   end
 
   data['articles'].each do |a|
-    puts a['tags']
-    #puts a['slug']
-    #puts a['published_at']
+    articles.append a
   end
 rescue JSON::ParserError => e
   raise JSON::ParserError, "Invalid JSON in #{path}: #{e.message}"
 end
 
-load_zenn_json("zenn1.json")
-load_zenn_json("zenn2.json")
+def strip_front_matter(content)
+  if content =~ /\A---\s*\n/
+    content.sub(/\A---\s*\n.*?\n---\s*\n/m, '')
+  else
+    content
+  end
+end
+
+def rewrite_image_paths(content)
+  content = content.gsub("](/images/", "](/assets/images/")
+  content
+end
+
+def export_article(article)
+  slug = article['slug']
+  filename = '../articles/'+slug+'.md'
+  content = File.read(filename)
+  content = strip_front_matter(content)
+  content = rewrite_image_paths(content)
+  fmatter = front_matter(article)
+  content = fmatter + content
+  export_file = 'mysite/_posts/' + slug + '.md'
+  puts export_file
+  File.write(export_file, content)
+end
+
+def front_matter(article)
+  fmatter = <<-"EOS"
+---
+layout: post
+title: #{article['title']}
+tags: [zenn]
+permalink: #{article['slug']}
+---
+
+# #{article['title']}
+
+  EOS
+  fmatter
+end
+
+articles = []
+
+load_zenn_json("zenn1.json", articles)
+load_zenn_json("zenn2.json", articles)
+
+articles.each do |article|
+  export_article(article)
+end
